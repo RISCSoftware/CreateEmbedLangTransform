60243ddb0527d756a9488ba9d788ba2ddbfc0400
Joachim Mairboeck <joachim.mairboeck@risc-software.at>
2023-05-15 12:37:54 +0000

    CreateEmbedLangTransform improvements
     * add additional language identifiers to the Template property in summary information
       * Wix 4 doesn't support doing this as part of the build any more (see https://github.com/orgs/wixtoolset/discussions/7325), so we have to do it in post processing (we are using an undocumented feature of Windows Installer here)
     * modernize: use unique_ptr instead of new/delete
     * improve error handling
     * switch build to Visual Studio 2022
    
    git-svn-id: https://poroz/svnad/wfl_mr/branches/dev/trunk/src/tools/CreateEmbedLangTransform@13646 0d26eecb-4107-0410-bdd6-a17e8e2aa929

--- CreateEmbedLangTransform/main.cpp
+++ CreateEmbedLangTransform/main.cpp
@@ -2,6 +2,7 @@
 #include <tchar.h>
 #include <MsiQuery.h>
 #include <iostream>
+#include <memory>
 
 #pragma comment(lib, "msi.lib")
 
@@ -12,29 +13,30 @@
 #endif // _UNICODE
 
 void HandleWindowsError(LPCTSTR func) {
-  DWORD error = GetLastError();
+  DWORD error{GetLastError()};
+  if (error == ERROR_SUCCESS) return;
   LPTSTR errorMessage;
-  FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPTSTR>(&errorMessage), 0, NULL);
-  std::tcerr << TEXT("error at ") << func << TEXT(": ") << errorMessage << std::endl;
+  FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPTSTR>(&errorMessage), 0u, nullptr);
+  std::tcerr << TEXT("Windows error at ") << func << TEXT(": ") << errorMessage << TEXT(" (") << error << TEXT(")") << std::endl;
   LocalFree(errorMessage);
 }
 
-void HandleMsiError(LPCTSTR func) {
+void HandleMsiError(UINT result, LPCTSTR func) {
   PMSIHANDLE error = MsiGetLastErrorRecord();
   if (error) {
-    DWORD length = 0;
-    UINT status = MsiFormatRecord(0, error, TEXT(""), &length);
+    DWORD length{0u};
+    auto status{MsiFormatRecord(0u, error, TEXT(""), &length)}; // get the length of the error message
     if (status == ERROR_MORE_DATA) {
-      length++;
-      LPTSTR errorMessage = new TCHAR[length];
-      status = MsiFormatRecord(0, error, errorMessage, &length);
+      length++; // for the '\0' terminator
+      auto errorMessage{std::make_unique<TCHAR[]>(length)};
+      status = MsiFormatRecord(0u, error, errorMessage.get(), &length);
       if (status == ERROR_SUCCESS) {
-        std::tcerr << TEXT("error at ") << func << TEXT(": ") << errorMessage << std::endl;
+        std::tcerr << TEXT("MSI error at ") << func << TEXT(": ") << errorMessage << TEXT(" (") << result << TEXT(")") << std::endl;
       }
-      delete[] errorMessage;
     }
   }
-  else {
+  else { // no error record, just print the error code
+    std::tcerr << TEXT("MSI error at ") << func << TEXT(" (") << result << TEXT(")") << std::endl;
     HandleWindowsError(func);
   }
 }
@@ -46,18 +48,18 @@ int _tmain(int argc, TCHAR **argv) {
     return 1;
   }
 
-  PMSIHANDLE source, target, view, record;
-  UINT uResult;
-  DWORD dwResult;
+  PMSIHANDLE source, target, view, record, summaryInfo;
+  UINT uResult, dataType;
+  DWORD dwResult, length{0u};
   BOOL bResult;
   TCHAR tempPath[MAX_PATH], tempFile[MAX_PATH];
 
   dwResult = GetTempPath(MAX_PATH, tempPath);
-  if (dwResult > MAX_PATH || dwResult == 0) {
+  if (dwResult > MAX_PATH || dwResult == 0u) {
     HandleWindowsError(TEXT("GetTempPath"));
     return 1;
   }
-  uResult = GetTempFileName(tempPath, TEXT("CELT"), 0, tempFile);
+  uResult = GetTempFileName(tempPath, TEXT("CELT"), 0u, tempFile);
   if (uResult == 0) {
     HandleWindowsError(TEXT("GetTempFileName"));
     return 1;
@@ -65,27 +67,27 @@ int _tmain(int argc, TCHAR **argv) {
 
   uResult = MsiOpenDatabase(argv[1], MSIDBOPEN_TRANSACT, &target);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiOpenDatabase target"));
+    HandleMsiError(uResult, TEXT("MsiOpenDatabase target"));
     return 1;
   }
   uResult = MsiOpenDatabase(argv[2], MSIDBOPEN_READONLY, &source);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiOpenDatabase source"));
+    HandleMsiError(uResult, TEXT("MsiOpenDatabase source"));
     return 1;
   }
   uResult = MsiDatabaseGenerateTransform(source, target, tempFile, 0, 0);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiDatabaseGenerateTransform"));
+    HandleMsiError(uResult, TEXT("MsiDatabaseGenerateTransform"));
     return 1;
   }
   uResult = MsiCreateTransformSummaryInfo(source, target, tempFile, 0, 0);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiCreateTransformSummaryInfo"));
+    HandleMsiError(uResult, TEXT("MsiCreateTransformSummaryInfo"));
     return 1;
   }
   uResult = MsiDatabaseOpenView(target, TEXT("SELECT `Name`,`Data` FROM _Storages"), &view);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiDatabaseOpenView"));
+    HandleMsiError(uResult, TEXT("MsiDatabaseOpenView"));
     return 1;
   }
   record = MsiCreateRecord(2);
@@ -93,29 +95,62 @@ int _tmain(int argc, TCHAR **argv) {
     std::tcerr << TEXT("error at MsiCreateRecord") << std::endl;
     return 1;
   }
-  uResult = MsiRecordSetString(record, 1, argv[3]);
+  uResult = MsiRecordSetString(record, 1u, argv[3]);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiRecordSetString substorage-name"));
+    HandleMsiError(uResult, TEXT("MsiRecordSetString substorage-name"));
     return 1;
   }
   uResult = MsiViewExecute(view, record);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiViewExecute"));
+    HandleMsiError(uResult, TEXT("MsiViewExecute"));
     return 1;
   }
-  uResult = MsiRecordSetStream(record, 2, tempFile);
+  uResult = MsiRecordSetStream(record, 2u, tempFile);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiRecordSetStream substorage-content"));
+    HandleMsiError(uResult, TEXT("MsiRecordSetStream substorage-content"));
     return 1;
   }
   uResult = MsiViewModify(view, MSIMODIFY_ASSIGN, record);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiViewModify"));
+    HandleMsiError(uResult, TEXT("MsiViewModify"));
+    return 1;
+  }
+  uResult = MsiGetSummaryInformation(target, nullptr, 1u, &summaryInfo);
+  if (uResult != ERROR_SUCCESS) {
+    HandleMsiError(uResult, TEXT("MsiGetSummaryInformation"));
+    return 1;
+  }
+  uResult = MsiSummaryInfoGetProperty(summaryInfo, PIDSI_TEMPLATE, &dataType, nullptr, nullptr, TEXT(""), &length); // get the length of the stored value
+  if (uResult != ERROR_MORE_DATA) {
+    HandleMsiError(uResult, TEXT("MsiSummaryInfoGetProperty length"));
+    return 1;
+  }
+  length++; // for the '\0' terminator
+  {
+    auto stringValue{std::make_unique<TCHAR[]>(length + _tcslen(argv[3]) + 1)}; // allocate a buffer long enough for the new value (including the separator)
+    uResult = MsiSummaryInfoGetProperty(summaryInfo, PIDSI_TEMPLATE, &dataType, nullptr, nullptr, stringValue.get(), &length);
+    if (uResult != ERROR_SUCCESS) {
+      HandleMsiError(uResult, TEXT("MsiSummaryInfoGetProperty value"));
+      return 1;
+    }
+    if (_tcsstr(stringValue.get(), argv[3]) == nullptr) { // check if the required value is already included, only add it if not
+      stringValue[length] = TEXT(',');
+      _tcscpy(stringValue.get() + length + 1, argv[3]);
+      uResult = MsiSummaryInfoSetProperty(summaryInfo, PIDSI_TEMPLATE, dataType, 0, nullptr, stringValue.get());
+      if (uResult != ERROR_SUCCESS) {
+        HandleMsiError(uResult, TEXT("MsiSummaryInfoSetProperty"));
+        return 1;
+      }
+    }
+  }
+  uResult = MsiSummaryInfoPersist(summaryInfo);
+  if (uResult != ERROR_SUCCESS) {
+    HandleMsiError(uResult, TEXT("MsiSummaryInfoPersist"));
     return 1;
   }
   uResult = MsiDatabaseCommit(target);
   if (uResult != ERROR_SUCCESS) {
-    HandleMsiError(TEXT("MsiDatabaseCommit target"));
+    HandleMsiError(uResult, TEXT("MsiDatabaseCommit target"));
     return 1;
   }
 
